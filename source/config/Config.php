50b9d382739b2252c6ee34f757fa97d4ecbf1186
Nixy <lunawillerp@gmail.com>
2022-08-04 12:03:53 -0300

    Loading Influence Domination
    Another Dominion Authority

--- source/config/Config.php
+++ source/config/Config.php
@@ -1,5 +1,5 @@
 <?php
     session_start();
     define("VERSION", "1.0");
-    define("URL_BASE", "http://localhost/Agamenon/");
+    define("URL_BASE", "https://goldfish-app-zv7bq.ondigitalocean.app/");
     define("SITE_TITLE", "...");
